1d3724d1a8bd7307d28581a395e0c356733ede83
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-01-10 12:11:41 +0100

    Remote Search Service Added

--- app/src/main/java/irawan/electroshock/doaku/api/DataService.kt
+++ app/src/main/java/irawan/electroshock/doaku/api/DataService.kt
@@ -3,8 +3,14 @@ package irawan.electroshock.doaku.api
 import irawan.electroshock.doaku.model.SerializedModel
 import retrofit2.Response
 import retrofit2.http.GET
+import okhttp3.ResponseBody
+import retrofit2.http.Path
+
 
 interface DataService {
     @GET("/api")
     suspend fun getAllData() : Response<List<SerializedModel>>
+
+    @GET("/api/doa/{search}")
+    fun geSearchByDoa(@Path(value = "search", encoded = true) name: String?): Response<List<SerializedModel>?>?
 }
