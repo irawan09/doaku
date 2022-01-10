1d5b8b0fbbd88e629b168ce30f2e173e275fce30
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-01-10 12:42:33 +0100

    Remote Search Service Added

--- app/src/main/java/irawan/electroshock/doaku/repository/DataRepository.kt
+++ app/src/main/java/irawan/electroshock/doaku/repository/DataRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.gson.GsonBuilder
 import irawan.electroshock.doaku.api.ServiceProvider
 import irawan.electroshock.doaku.database.DoaDatabaseFactory
 import irawan.electroshock.doaku.model.DatabaseModel
@@ -14,11 +15,13 @@ import kotlinx.coroutines.withContext
 
 class DataRepository(context: Context) {
     private var doaResponseLiveData : MutableLiveData<List<DatabaseModel>>? = null
+    private var doaResponseSearchLiveData : MutableLiveData<List<DatabaseModel>>? = null
     private var databaseResponseData : LiveData<List<DatabaseModel>>? = null
     private var doaArray : ArrayList<DatabaseModel> = ArrayList()
 
     init {
         doaResponseLiveData = MutableLiveData<List<DatabaseModel>>()
+        doaResponseSearchLiveData = MutableLiveData<List<DatabaseModel>>()
         loadAllData(context)
         databaseResponseData = DoaDatabaseFactory.getDatabaseInstance(context).doaDao().getAllDoa()
     }
@@ -58,6 +61,28 @@ class DataRepository(context: Context) {
                 }
             }
         }
+    }
+
+    fun SearchDoa(context: Context, query : String){
+        val service = ServiceProvider(context).createService()
+        CoroutineScope(Dispatchers.IO).launch{
+            val response = service.geSearchByDoa(query)
+            withContext(Dispatchers.Main){
+                if (response.isSuccessful){
+                    val data = response.body()
+                    val gson = GsonBuilder().setPrettyPrinting().create()
+                    val prettyJson = gson.toJson(data)
+                    Log.d("Pretty JSON response ", prettyJson)
+                } else {
+                    Log.e("Status", "Retrofit Error ${response.code()}")
+                }
+            }
+
+        }
+    }
+
+    fun getDoaResponseSearchLiveData(): LiveData<List<DatabaseModel>>? {
+        return doaResponseSearchLiveData
     }
 
     fun getDoaResponseLiveData() : LiveData<List<DatabaseModel>>? {
