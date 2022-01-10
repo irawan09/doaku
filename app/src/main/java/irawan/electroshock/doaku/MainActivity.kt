d048e3a5cdb334fb8e7488e7f144e8a14e37c9b3
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-01-10 01:33:39 +0100

    Update Doa Details UI

--- app/src/main/java/irawan/electroshock/doaku/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/doaku/MainActivity.kt
@@ -1,5 +1,6 @@
 package irawan.electroshock.doaku
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,18 +10,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
 import dagger.hilt.android.AndroidEntryPoint
+import irawan.electroshock.doaku.model.DatabaseModel
 import irawan.electroshock.doaku.ui.theme.DoakuTheme
 import irawan.electroshock.doaku.utils.NetworkMonitor
 import irawan.electroshock.doaku.utils.Utils
 import irawan.electroshock.doaku.view.NavigationController
 import irawan.electroshock.doaku.view_model.DataViewModel
 
+@ExperimentalComposeUiApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val dataViewModel : DataViewModel by viewModels<DataViewModel>()
     private lateinit var networkMonitor : NetworkMonitor
 
-    @OptIn(ExperimentalComposeUiApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Utils.setLifeCycleOwner(this)
@@ -28,26 +30,24 @@ class MainActivity : ComponentActivity() {
         networkMonitor.observe(this, { network ->
             if(network == true){
                 dataViewModel.getRemoteResponseLiveData()?.observe(this, { data ->
-                    setContent {
-                        DoakuTheme {
-                            Surface(color = MaterialTheme.colors.background) {
-                                    NavigationController(this, network, data)
-                            }
-                        }
-                    }
+                    layout(this, network, data)
                 })
             } else{
                 Toast.makeText(this,"Load Data from Database !!", Toast.LENGTH_LONG).show()
                 dataViewModel.getDatabaseResponseLiveData()?.observe(this, {
-                    setContent {
-                        DoakuTheme {
-                            Surface(color = MaterialTheme.colors.background) {
-                                    NavigationController(this, network, it)
-                            }
-                        }
-                    }
+                    layout(this, network, it)
                 })
             }
         })
+    }
+
+    private fun layout(context : Context, network : Boolean, dataViewModel: List<DatabaseModel>){
+        setContent {
+            DoakuTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    NavigationController(context, network, dataViewModel)
+                }
+            }
+        }
     }
 }
