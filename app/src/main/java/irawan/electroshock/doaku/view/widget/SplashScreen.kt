1e68a4e968af09a503e2f2e32071456f1fc1b61e
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-01-18 11:34:06 +0100

    Update UI

--- app/src/main/java/irawan/electroshock/doaku/view/widget/SplashScreen.kt
+++ app/src/main/java/irawan/electroshock/doaku/view/widget/SplashScreen.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.delay
 @Composable
 fun AnimatedSplashScreen(navController: NavController){
     LaunchedEffect(key1 = true){
-        delay(1000L)
+        delay(950L)
         navController.popBackStack()
         navController.navigate("DoaListFragment")
     }
