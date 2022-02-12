dc635515fbe3636bbd02d7acece183a28060829c
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-02-12 05:37:49 +0100

    Gradle Update

--- app/src/main/java/irawan/electroshock/doaku/MainActivity.kt
+++ app/src/main/java/irawan/electroshock/doaku/MainActivity.kt
@@ -1,23 +1,22 @@
 package irawan.electroshock.doaku
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.graphics.Color
 import coil.annotation.ExperimentalCoilApi
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
 import irawan.electroshock.doaku.model.DatabaseModel
-import irawan.electroshock.doaku.view.theme.DoakuTheme
 import irawan.electroshock.doaku.utils.NetworkMonitor
 import irawan.electroshock.doaku.utils.Utils
 import irawan.electroshock.doaku.view.navigation.NavigationController
 import irawan.electroshock.doaku.view.navigation.OnBoardingController
+import irawan.electroshock.doaku.view.theme.DoakuTheme
 import irawan.electroshock.doaku.view.widget.ShowAlertDialog
 import irawan.electroshock.doaku.view_model.DataViewModel
 import irawan.electroshock.doaku.view_model.OnBoardViewModel
@@ -30,7 +29,7 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val dataViewModel : DataViewModel by viewModels<DataViewModel>()
-    private val onBoardViewModel : OnBoardViewModel by viewModels<OnBoardViewModel>()
+    private val onBoardViewModel : OnBoardViewModel by viewModels()
     private lateinit var networkMonitor : NetworkMonitor
 
     @ExperimentalCoilApi
