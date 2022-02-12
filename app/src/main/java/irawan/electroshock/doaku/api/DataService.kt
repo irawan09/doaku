b8885250212e780a7fd8a97ba4f7e7c3c07b1bf3
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-02-12 03:41:07 +0100

    lastest version

--- app/src/main/java/irawan/electroshock/doaku/api/DataService.kt
+++ app/src/main/java/irawan/electroshock/doaku/api/DataService.kt
@@ -1,10 +1,8 @@
 package irawan.electroshock.doaku.api
 
 import irawan.electroshock.doaku.model.JSONObject
-import irawan.electroshock.doaku.model.SerializedModel
 import retrofit2.Response
 import retrofit2.http.GET
-import okhttp3.ResponseBody
 import retrofit2.http.Path
 
 
